2cbf7618fc41a37f3a2a6f706e1913ebca333ad7
xiaobo <henaxiaobo168@163.com>
2019-06-09 18:15:01 +0800

    config

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+	<div>
+		elem!!!
+    </div>
+</template>
+
+<script>
+  	export default {
+  	}
+
+</script>
+
+<style lang="scss">
+	.router-fade-enter-active, .router-fade-leave-active {
+	  	transition: opacity .3s;
+	}
+	.router-fade-enter, .router-fade-leave-active {
+	  	opacity: 0;
+	}
+</style>
---
+++ src/main.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import App from './App'
+
+
+new Vue({
+	components: {
+		App	
+	},
+	template: '<App/>'
+}).$mount('#app')
+
