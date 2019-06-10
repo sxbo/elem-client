06bfc70ba4e271012c596c758cb9a33703cae074
xiaobo <henaxiaobo168@163.com>
2019-06-11 00:27:37 +0800

    home页面

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,23 @@
 <template>
 	<div>
-		elem!!!
+		<transition name="router-fade" mode="out-in">
+			<keep-alive>
+			    <router-view v-if="$route.meta.keepAlive"></router-view>
+			</keep-alive>
+    	</transition>
+    	<transition name="router-fade" mode="out-in">
+			<router-view v-if="!$route.meta.keepAlive"></router-view>
+		</transition>
+		<svg-icon></svg-icon>
     </div>
 </template>
 
 <script>
+	import svgIcon from './components/common/svg';
   	export default {
+		components:{
+            svgIcon
+        },
   	}
 
 </script>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import App from './App'
+import VueRouter from 'vue-router'
+import routes from './router/router'
 
-
+Vue.use(VueRouter)
+const router = new VueRouter({
+	routes,
+})
 new Vue({
-	components: {
-		App	
-	},
-	template: '<App/>'
+	router	
 }).$mount('#app')
 
