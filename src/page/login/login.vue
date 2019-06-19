79816889471300b38c4740c7f68df23b202ecd55
xiaobo <henaxiaobo168@163.com>
2019-06-20 00:16:37 +0800

    登录注册页面完成

--- src/page/login/login.vue
+++ src/page/login/login.vue
@@ -1,8 +1,49 @@
 <template>
     <div class="loginContainer">
         <head-Top :head-title="loginWay?'登录':'密码登录'" goBack="true">
-
         </head-Top>
+        <form class="loginForm" v-if="loginWay">
+            <section class="input_container phone_number">
+                <input type="text" placeholder="手机号码" name="phone" maxlength="11">
+                <button>获取验证码</button>
+                <button>已发送 s</button>
+            </section>
+            <section class="input_container">
+                <input type="text" placeholder="验证码" name="mobileCode" maxlength="6" v-model="mobileCode">
+            </section>
+        </form>
+        <form class="loginForm" v-else>
+            <section class="input_container">
+                <input type="text" placeholder="账号" >
+            </section>
+            <section class="input_container">
+                <input type="password" placeholder="密码">
+                <div class="button_switch">
+                    <div class="circle_button"></div>
+                    <span>abc</span>
+                    <span>...</span>
+                </div>
+            </section>
+            <section class="input_container captcha_code_container">
+                <input type="text" placeholder="验证码" maxlength="4">
+                <div class="img_change_img">
+                    <img src="" alt="图">
+                    <div class="change_img">
+                        <p>看不清</p>
+                        <p>换一张</p>
+                    </div>
+                </div>
+            </section>
+        </form>
+        <p class="login_tips">
+            温馨提示：未注册过的账号，登录时将自动注册
+        </p>
+        <p class="login_tips">
+            注册过的账号可凭账号密码登录
+        </p>
+        <div class="login_container">登录</div>
+        <router-link to="/forget" class="to_forget" v-if="!loginWay">重置密码？</router-link>
+
     </div>
 </template>
 
@@ -30,6 +71,75 @@ export default {
         p, span, input{
             font-family: Helvetica Neue,Tahoma,Arial;
         }
+    }
+
+    .loginForm{
+        background-color: #fff;
+        margin-top: .6rem;
+        .input_container{
+            display: flex;
+            justify-content: space-between;
+            padding: .6rem .8rem;
+            border-bottom: 1px solid #f1f1f1;
+            input{
+                @include sc(.7rem, #666);
+            }
+            button{
+                @include sc(.65rem, #fff);
+                font-family: Helvetica Neue,Tahoma,Arial;
+                padding: .28rem .4rem;
+                border: 1px;
+                border-radius: 0.15rem;
+            }
+            .right_phone_number{
+                background-color: #4cd964;
+            }
+        }
+        .phone_number{
+            padding: .3rem .8rem;
+        }
+        .captcha_code_container{
+            height: 2.2rem;
+            .img_change_img{
+                display: flex;
+                align-items: center;
+                img{
+                    @include wh(3.5rem, 1.5rem);
+                    margin-right: .2rem;
+                }
+                .change_img{
+                    display: flex;
+                    flex-direction: column;
+                    flex-wrap: wrap;
+                    width: 2rem;
+                    justify-content: center;
+                    p{
+                        @include sc(.55rem, #666);
+                    }
+                    p:nth-of-type(2){
+                        color: #3b95e9;
+                        margin-top: .2rem;
+                    }
+                }
+            }
+        }
+    }
+    .login_tips{
+        @include sc(.5rem, red);
+        padding: .4rem .6rem;
+        line-height: .5rem;
+        a{
+            color: #3b95e9;
+        }
+    }
+    .login_container{
+        margin: 0 .5rem 1rem;
+        @include sc(.7rem, #fff);
+        background-color: #4cd964;
+        padding: .5rem 0;
+        border: 1px;
+        border-radius: 0.15rem;
+        text-align: center;
     }
 </style>
 
