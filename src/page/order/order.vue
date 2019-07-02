1cbb6e4f165794a628f751d47c8d9706f0d54860
xiaobo <henaxiaobo168@163.com>
2019-07-03 00:11:39 +0800

    新增订单初始页面

---
+++ src/page/order/order.vue
@@ -0,0 +1,21 @@
+<template>
+    
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            orderList: null,      //订单列表
+            offset: 0,            //列表起始标志位
+            preventRepeat: false,  //防止重复获取
+            showLoading: true,    //显示加载动画
+            imgBaseUrl: ''        //图片加载地址
+        }
+    }, 
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/page/profile/profile.vue
@@ -0,0 +1,24 @@
+<template>
+    
+</template>
+<script>
+export default {
+    data() {
+        return {
+            profiletitle: '我的',   //title
+            username: '登录注册',    //用户名
+            resetname: '',          //重置用户名
+            mobile: '暂无绑定手机号', //电话
+            balance: 0,              //余额
+            count: 0,                //优惠卷个数
+            pointNumber: 0,           //积分数
+            avatar: '',              // 头像地址
+            imgBaseUrl: '',          //图片加载地址
+        }
+    }    
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
